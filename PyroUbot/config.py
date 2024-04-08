32dbc0c4f73f4b4abfea70c85daa6743844f8389
Wildan <155066169+Muhamadwildan018@users.noreply.github.com>
2024-04-09 04:02:36 +0700

    config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -4,11 +4,11 @@ DEVS = [
     5779185981,
 ]
 
-API_ID = int(os.getenv("API_ID", "27778109"))
+API_ID = int(os.getenv("API_ID", "26477680"))
 
-API_HASH = os.getenv("API_HASH", "6b0f4197f9f637e17a1466f4de587c07")
+API_HASH = os.getenv("API_HASH", "b0d8504752cc1ecf52009ece2bdef0b8")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7068443168:AAEJ9utUdGe3kuqvbqVz0XfwqiU1vR7lbjg")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7193533321:AAENiqf1KUMciDzVbOskRdeQd9o7ybE3-DE")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "5779185981"))
 
@@ -16,7 +16,7 @@ LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002032983910"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002127258037").split()))
 
-MAX_BOT = int(os.getenv("MAX_BOT", "50"))
+MAX_BOT = int(os.getenv("MAX_BOT", "550"))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
