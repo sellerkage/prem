0c420cca91d7b8d9fcd2404d2aa7dcac6a52287d
Wildan <155066169+Muhamadwildan018@users.noreply.github.com>
2024-04-10 03:51:09 +0700

    str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -27,14 +27,13 @@ async def ping_cmd(client, message):
     else:
         _ping = f"""
 <b>❏ᴘᴏɴɢ:</b> <code>{str(delta_ping).replace('.', ',')} ms</code>
-<b>⏰ᴜᴘᴛɪᴍᴇ:</b> <code>{str(delta_time).replace('.', ',')} ms</code>
 <b>👑ᴍᴇɴᴛɪᴏɴ:</b> <code>@{bot.me.username}</code>
 """
     await message.reply(_ping)
 
 
 async def start_cmd(client, message):
-    if len(message.command) < 3:
+    if len(message.command) < 2:
         buttons = Button.start(message)
         msg = MSG.START(message)
         await message.reply(msg, reply_markup=InlineKeyboardMarkup(buttons))
