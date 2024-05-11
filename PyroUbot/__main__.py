309d26e1eddd4fd32b0f2688156725f290a79e8d
sellerkage <145161621+sellerkage@users.noreply.github.com>
2024-05-11 17:18:53 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -3,37 +3,52 @@ import asyncio
 from pyrogram import idle
 
 from PyroUbot import *
+from PyroUbot.core.function.expired import *
 
 
-async def start_ubot(user_id, _ubot):
-    ubot_ = Ubot(**_ubot)
+async def handle_timeout_error(user_id):
+    await remove_ubot(user_id)
+    await add_prem(user_id)
+    await rm_all(user_id)
+    await rem_pref(user_id)
+    await rem_uptime(user_id)
+    for chat_id in await get_chat(user_id):
+        await remove_chat(user_id, chat_id)
+    await sending_user(user_id)
+    print(f"[INFO] - ({user_id}) TIMEOUT ERROR - BOT REMOVED")
+
+
+async def handle_general_error(user_id, error):
+    await remove_ubot(user_id)
+    await rm_all(user_id)
+    await rem_pref(user_id)
+    await rem_uptime(user_id)
+    await rem_expired_date(user_id)
+    for chat_id in await get_chat(user_id):
+        await remove_chat(user_id, chat_id)
+    print(f"✅ {user_id} BOT ERROR: {error}")
+
+
+async def start_user_bot(user_id, userbot_data):
+    ubot = Ubot(**userbot_data)
     try:
-        await asyncio.wait_for(ubot_.start(), timeout=30)
-        await ubot_.join_chat("Disney_storeDan")
-
+        await asyncio.wait_for(ubot.start(), timeout=30)
+        chat_list = ["suportkage", "kagestore69"]
+        for chat_name in chat_list:
+            await ubot.join_chat(chat_name)
     except asyncio.TimeoutError:
-        await remove_ubot(user_id)
-        await add_prem(user_id)
-        await sending_user(user_id)
-        print(f"[ɪɴꜰᴏ] - ({user_id}) ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇʀᴇꜱᴘᴏɴ")
-    except:
-        await remove_ubot(user_id)
-        await rm_all(user_id)
-        await remove_all_vars(user_id)
-        await rem_pref(user_id)
-        await rem_expired_date(user_id)
-        for X in await get_chat(user_id):
-            await remove_chat(user_id, X)
-        print(f"✅ {user_id} ʙᴇʀʜᴀꜱɪʟ ᴅɪʜᴀᴘᴜꜱ")
+        await handle_timeout_error(user_id)
+    except Exception as e:
+        await handle_general_error(user_id, e)
 
 
 async def main():
     tasks = [
-        asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot))
-        for _ubot in await get_userbots()
+        asyncio.create_task(start_user_bot(int(userbot_data["name"]), userbot_data))
+        for userbot_data in await get_userbots()
     ]
     await asyncio.gather(*tasks, bot.start())
-    await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
+    await asyncio.gather(loadPlugins(), expiredUserbots(), idle())
 
 
 if __name__ == "__main__":
