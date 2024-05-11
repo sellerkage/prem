97bf1833445805369d3c79aa4853f35856552f89
sellerkage <145161621+sellerkage@users.noreply.github.com>
2024-05-11 17:10:08 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,21 +1,29 @@
+import asyncio
 import logging
 import os
 import re
 
-from pyrogram import Client, filters
-from pyrogram.enums import ParseMode
-from pyrogram.handlers import CallbackQueryHandler, MessageHandler
-from pyrogram.types import Message
+from aiohttp import ClientSession
+from pyrogram import *
+from pyrogram.enums import *
+from pyrogram.handlers import *
+from pyrogram.types import *
 from pyromod import listen
 
 from PyroUbot.config import *
 
+aiosession = ClientSession()
+
+import logging
+from os import execvp
+from sys import executable
+
 
 class ConnectionHandler(logging.Handler):
     def emit(self, record):
-        for X in ["OSErro", "TimeoutError"]:
+        for X in ["OSError"]:
             if X in record.getMessage():
-                os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
+                execvp(executable, [executable, "-m", "PyroUbot"])
 
 
 logger = logging.getLogger()
@@ -31,9 +39,107 @@ logger.addHandler(stream_handler)
 logger.addHandler(connection_handler)
 
 
+class Ubot(Client):
+    _ubot = []
+    _prefix = {}
+    _get_my_id = []
+    _translate = {}
+    # _get_my_peer = {}
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.device_model = "kageimuet"
+
+    def on_message(self, filters=None, group=-1):
+        def decorator(func):
+            for ub in self._ubot:
+                ub.add_handler(MessageHandler(func, filters), group)
+            return func
+
+        return decorator
+
+    def set_prefix(self, user_id, prefix):
+        self._prefix[self.me.id] = prefix
+
+    async def start(self):
+        await super().start()
+        handler = await get_pref(self.me.id)
+        if handler:
+            self._prefix[self.me.id] = handler
+        else:
+            self._prefix[self.me.id] = ["."]
+        self._ubot.append(self)
+        self._get_my_id.append(self.me.id)
+        self._translate[self.me.id] = {"negara": "id"}
+        print(f"Starting Userbot ({self.me.id}|{self.me.first_name})")
+
+    async def stop(self):
+        await super().stop()
+
+
+async def get_prefix(user_id):
+    return ubot._prefix.get(user_id, ".")
+
+
+def anjay(cmd):
+    command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
+
+    async def func(_, client, message):
+        if message.text and message.from_user:
+            text = message.text.strip()
+            username = client.me.username or ""
+            prefixes = await get_prefix(client.me.id)
+
+            if not text:
+                return False
+
+            for prefix in prefixes:
+                if not text.startswith(prefix):
+                    continue
+
+                without_prefix = text[len(prefix) :]
+
+                for command in cmd.split("|"):
+                    if not re.match(
+                        rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
+                        without_prefix,
+                        flags=re.IGNORECASE | re.UNICODE,
+                    ):
+                        continue
+
+                    without_command = re.sub(
+                        rf"{command}(?:@?{username})?\s?",
+                        "",
+                        without_prefix,
+                        count=1,
+                        flags=re.IGNORECASE | re.UNICODE,
+                    )
+                    message.command = [command] + [
+                        re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
+                        for m in command_re.finditer(without_command)
+                    ]
+
+                    return True
+
+        return False
+
+    return filters.create(func)
+
+
+ubot = Ubot(name="ubot", api_id=API_ID, api_hash=API_HASH, device_model="kage")
+
+
 class Bot(Client):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, device_model="ᴜꜱᴇʀʙᴏᴛ")
+        super().__init__(
+            **kwargs,
+            name="bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=20,
+            device_model="KageLoveyou",
+        )
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
@@ -53,98 +159,10 @@ class Bot(Client):
         await super().start()
 
 
-class Ubot(Client):
-    _ubot = []
-    _prefix = {}
-    _get_my_id = []
-    _translate = {}
-    _get_my_peer = {}
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs, device_model="ᴜꜱᴇʀʙᴏᴛ")
-
-    def on_message(self, filters=None, group=-1):
-        def decorator(func):
-            for ub in self._ubot:
-                ub.add_handler(MessageHandler(func, filters), group)
-            return func
-
-        return decorator
-
-    def set_prefix(self, user_id, prefix):
-        self._prefix[user_id] = prefix
-
-    async def get_prefix(self, user_id):
-        return self._prefix.get(user_id, ["."])
-
-    def cmd_prefix(self, cmd):
-        command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
-
-        async def func(_, client, message):
-            if message.text:
-                text = message.text.strip().encode("utf-8").decode("utf-8")
-                username = client.me.username or ""
-                prefixes = await self.get_prefix(client.me.id)
-
-                if not text:
-                    return False
-
-                for prefix in prefixes:
-                    if not text.startswith(prefix):
-                        continue
-
-                    without_prefix = text[len(prefix) :]
-
-                    for command in cmd.split("|"):
-                        if not re.match(
-                            rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
-                            without_prefix,
-                            flags=re.IGNORECASE | re.UNICODE,
-                        ):
-                            continue
-
-                        without_command = re.sub(
-                            rf"{command}(?:@?{username})?\s?",
-                            "",
-                            without_prefix,
-                            count=1,
-                            flags=re.IGNORECASE | re.UNICODE,
-                        )
-                        message.command = [command] + [
-                            re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
-                            for m in command_re.finditer(without_command)
-                        ]
-
-                        return True
-
-                return False
-
-        return filters.create(func)
-
-    async def start(self):
-        await super().start()
-        handler = await get_pref(self.me.id)
-        if handler:
-            self._prefix[self.me.id] = handler
-        else:
-            self._prefix[self.me.id] = ["."]
-        self._ubot.append(self)
-        self._get_my_id.append(self.me.id)
-        self._translate[self.me.id] = "id"
-        print(f"[ɪɴꜰᴏ] - ({self.me.id}) - ꜱᴛᴀʀᴛᴇᴅ")
-
-
-bot = Bot(
-    name="bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-)
-
-ubot = Ubot(name="ubot")
+bot = Bot()
 
 
 from PyroUbot.core.database import *
-from PyroUbot.core.function import *
+from PyroUbot.core.function.plugins import *
 from PyroUbot.core.helpers import *
 from PyroUbot.core.plugins import *
