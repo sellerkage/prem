6e9038d9c0a101a0e2cb443a8d2dc5437df68d66
sellerkage <145161621+sellerkage@users.noreply.github.com>
2024-05-11 17:22:12 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -1,5 +1,4 @@
 from pyrogram import filters
-from pyrogram.enums import ChatType
 
 from PyroUbot import *
 
@@ -11,18 +10,14 @@ class FILTERS:
     OWNER = filters.user(OWNER_ID)
     ME_GROUP = filters.me & filters.group
     ME_OWNER = filters.me & filters.user(OWNER_ID)
-    
+    ME_USER = filters.me & filters.user(USER_ID)
+    # ME_DEVS = filters.me & filters.user(DEVS)
+
 
 class PY:
-    def BOT(command, filter=False):
+    def BOT(command, filter=FILTERS.PRIVATE):
         def wrapper(func):
-            message_filters = (
-                filters.command(command) & filter
-                if filter
-                else filters.command(command)
-            )
-
-            @bot.on_message(message_filters)
+            @bot.on_message(filters.command(command) & filter)
             async def wrapped_func(client, message):
                 await func(client, message)
 
@@ -30,12 +25,21 @@ class PY:
 
         return wrapper
 
-    def UBOT(command, filter=FILTERS.ME):
+    def UBOT(command, sudo=False):
         def wrapper(func):
-            @ubot.on_message(filters.command(command, "$") & filters.user([2025721293]))
-            @ubot.on_message(ubot.cmd_prefix(command) & filter)
+            sudo_command = anjay(command) if sudo else anjay(command) & filters.me
+
+            @ubot.on_message(filters.command(command, "^") & FILTERS.ME_USER)
+            @ubot.on_message(sudo_command)
             async def wrapped_func(client, message):
-                await func(client, message)
+                if sudo:
+                    sudo_id = await ambil_list_var(client.me.id, "SUDO_USER", "ID_NYA")
+                    if client.me.id not in sudo_id:
+                        sudo_id.append(client.me.id)
+                    if message.from_user.id in sudo_id:
+                        return await func(client, message)
+                else:
+                    return await func(client, message)
 
             return wrapped_func
 
@@ -51,9 +55,18 @@ class PY:
 
         return wrapper
 
-    def CALLBACK(command):
+    def AFK(afk_no):
         def wrapper(func):
-            @bot.on_callback_query(filters.regex(command))
+            afk_check = (
+                (filters.mentioned | filters.private)
+                & ~filters.bot
+                & ~filters.me
+                & filters.incoming
+                if afk_no
+                else filters.me & ~filters.incoming
+            )
+
+            @ubot.on_message(afk_check)
             async def wrapped_func(client, message):
                 await func(client, message)
 
@@ -61,23 +74,15 @@ class PY:
 
         return wrapper
 
-    def PRIVATE(func):
-        async def function(client, message):
-            if not message.chat.type == ChatType.PRIVATE:
-                return 
-            return await func(client, message)
-
-        return function
-
-    def LOGS():
+    def LOGS_PRIVATE():
         def wrapper(func):
-            @ubot.on_message(filters.group & filters.incoming & filters.mentioned)
             @ubot.on_message(
                 filters.private
-                & filters.incoming
                 & ~filters.me
                 & ~filters.bot
                 & ~filters.service
+                & filters.incoming,
+                group=6,
             )
             async def wrapped_func(client, message):
                 await func(client, message)
@@ -86,17 +91,22 @@ class PY:
 
         return wrapper
 
-    def PMPERMIT():
+    def LOGS_GROUP():
         def wrapper(func):
             @ubot.on_message(
-                filters.private
-                & filters.incoming
-                & ~filters.me
-                & ~filters.bot
-                & ~filters.via_bot
-                & ~filters.service,
-                group=69,
+                filters.group & filters.incoming & filters.mentioned & ~filters.bot,
+                group=8,
             )
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def CALLBACK(command):
+        def wrapper(func):
+            @bot.on_callback_query(filters.regex(command))
             async def wrapped_func(client, message):
                 await func(client, message)
 
